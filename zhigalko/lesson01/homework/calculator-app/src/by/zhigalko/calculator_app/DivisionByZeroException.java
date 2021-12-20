201e25a555fc383715d2cbdf59b7501c6ac0e400
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2021-12-20 20:52:34 +0300

    delete stranger directories

--- zhigalko/lesson01/homework/calculator-app/src/by/zhigalko/calculator_app/DivisionByZeroException.java
+++
@@ -1,9 +0,0 @@
-package by.zhigalko.calculator_app;
-
-public class DivisionByZeroException extends Exception {
-
-
-    public DivisionByZeroException(String message) {
-        super(message);
-    }
-}
