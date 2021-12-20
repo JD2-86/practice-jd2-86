7f21115099b7a3955c80f10f4dbbb51c0620ca62
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2021-12-20 11:47:52 +0300

    clear

--- shcharbunou/lesson02/maven-intro/src/main/java/by/it/shcharbunou/calculator/application/Runner.java
+++
@@ -1,9 +0,0 @@
-package by.it.shcharbunou.calculator.application;
-
-public class Runner {
-
-    public static void main(String[] args) {
-        CalculatorApplication application = new CalculatorApplication();
-        application.runApplication();
-    }
-}
