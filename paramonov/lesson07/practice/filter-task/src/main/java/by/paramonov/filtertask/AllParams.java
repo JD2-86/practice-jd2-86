e060fbec7be6da15b6fbae26e914f3c0964248b1
igor <gob1in@Mail.ru>
2021-12-22 20:02:27 +0300

    prantice 07lesson in class

---
+++ paramonov/lesson07/practice/filter-task/src/main/java/by/paramonov/filtertask/AllParams.java
@@ -0,0 +1,33 @@
+package by.paramonov.filtertask;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Set;
+
+@WebFilter(urlPatterns = "/*")
+public class AllParams implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        Enumeration<String> parameterNames = servletRequest.getParameterNames();
+        while (parameterNames.hasMoreElements()){
+            String s = parameterNames.nextElement();
+            System.out.println("название параметра : " + s);
+        };
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
---
+++ paramonov/lesson07/practice/filter-task/src/main/java/by/paramonov/filtertask/UnsafeFilter.java
@@ -0,0 +1,23 @@
+package by.paramonov.filtertask;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+@WebFilter(urlPatterns = "/unsafe")
+public class UnsafeFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
