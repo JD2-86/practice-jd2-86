0683671a2709b2dcf41066e2144938ca3d3b8b8f
AleksandrTolkach <shuuurick@mail.ru>
2021-12-24 17:38:10 +0300

    after cleaning add lesson05

---
+++ tolkach/lesson05/src/main/java/by/rolkach/view/WorkerWithFile.java
@@ -0,0 +1,16 @@
+package by.rolkach.view;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ * Class for getting file path.
+ */
+
+public class WorkerWithFile {
+
+    protected String getListOfUsers(HttpServletRequest request) {
+        return request.getSession()
+                .getServletContext()
+                .getRealPath("WEB-INF/ListOfUsers.txt");
+    }
+}
