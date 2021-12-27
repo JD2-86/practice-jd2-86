7215c908b32249c8892f0e34705eb01c22985a29
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2021-12-27 13:30:27 +0300

    feat: add solution for tasks 1-3

---
+++ shcharbunou/lesson05/user-management-web-application/src/main/java/by/shcharbunou/usermanagementwebapplication/servlets/task3/SaveUserServlet.java
@@ -0,0 +1,25 @@
+package by.shcharbunou.usermanagementwebapplication.servlets.task3;
+
+import by.shcharbunou.usermanagementwebapplication.entities.User;
+import by.shcharbunou.usermanagementwebapplication.repositories.UserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "SaveUserServlet", value = "/api/users")
+public class SaveUserServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstname");
+        String lastName = req.getParameter("lastname");
+        String email = req.getParameter("email");
+        String userName = req.getParameter("username");
+        String password = req.getParameter("password");
+        UserRepository db = UserRepository.getInstance();
+        User newUser = new User(db.getCounter().incrementAndGet(), firstName, lastName, email, userName, password);
+        db.putUserIntoDB(db.getCounter().get(), newUser);
+        resp.sendRedirect("../../user-management-web-application/view/users/?id=" + db.getCounter().toString());
+    }
+}
---
+++ shcharbunou/lesson05/user-management-web-application/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Management Web-Application</title>
+</head>
+<body>
+
+<p><a href="view/users/">Task 1: View user by ID</a></p>
+<p><a href="view/command/create-user">Task 2: Fake user create</a></p>
+<p><a href="createUserPost.jsp">Task 3: Real user create (saving to database)</a></p>
+
+</body>
+</html>
