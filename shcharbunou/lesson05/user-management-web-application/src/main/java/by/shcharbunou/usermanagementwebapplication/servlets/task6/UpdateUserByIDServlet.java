cad6b93a8f25db87e10ffb53b213f32eca927eeb
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2021-12-28 13:04:11 +0300

    feat: add solution for task 6

---
+++ shcharbunou/lesson05/user-management-web-application/src/main/java/by/shcharbunou/usermanagementwebapplication/servlets/task6/UpdateUserByIDServlet.java
@@ -0,0 +1,44 @@
+package by.shcharbunou.usermanagementwebapplication.servlets.task6;
+
+import by.shcharbunou.usermanagementwebapplication.entities.User;
+import by.shcharbunou.usermanagementwebapplication.repositories.UserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "UpdateUserByIDServlet", value = "/api/users/")
+public class UpdateUserByIDServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPut(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long id = (long) req.getAttribute("id");
+        String updatedFirstName = req.getParameter("firstname");
+        String updatedLastName = req.getParameter("lastname");
+        String updatedEmail = req.getParameter("email");
+        String updatedUsername = req.getParameter("username");
+        String updatedPassword = req.getParameter("password");
+
+        try (PrintWriter writer = resp.getWriter()) {
+            UserRepository db = UserRepository.getInstance();
+            if (db.findUserInDB(id)) {
+                User temporaryUser = db.getUserFromDB(id);
+                temporaryUser.setFirstName(updatedFirstName);
+                temporaryUser.setLastName(updatedLastName);
+                temporaryUser.setEmail(updatedEmail);
+                temporaryUser.setUsername(updatedUsername);
+                temporaryUser.setPassword(updatedPassword);
+                db.putUserIntoDB(id, temporaryUser);
+                resp.sendRedirect(req.getContextPath() + "/view/users/?id=" + id);
+            } else {
+                writer.write("<h1>Error: Something went wrong :/</h1>");
+            }
+        }
+    }
+}
