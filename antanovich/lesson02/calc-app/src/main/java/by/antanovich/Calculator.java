bbd2a7cdd9a0ea9852d9f88b99f68154b703c5e3
Valentina Antanovich <antanovichvalentina@gmail.com>
2021-12-29 20:07:26 +0300

    fix: delete unused classes

--- antanovich/lesson02/calc-app/src/main/java/by/antanovich/Calculator.java
+++ antanovich/lesson02/calc-app/src/main/java/by/antanovich/Calculator.java
@@ -1,11 +1,10 @@
 package by.antanovich;
 
 public class Calculator {
-
     public double add(double a, double b) {
         return a + b;
     }
-    
+
     public double subtract(double a, double b) {
         return a - b;
     }
--- antanovich/lesson02/calc-app/src/test/java/by/antanovich/CalculatorTest.java
+++ antanovich/lesson02/calc-app/src/test/java/by/antanovich/CalculatorTest.java
@@ -7,48 +7,42 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 public class CalculatorTest {
-
     private Calculator calculator = new Calculator();
- 
-@Test
-public void add() {
-    assertEquals(4, calculator.add(2, 2), 0);
-    assertEquals(1.42, calculator.add(3.14, -1.72), 0.001);
-    assertEquals(5.0/3, calculator.add(2.5/3, 2.5/3), 0.001);
-}
 
-@Test
-public void subtract() {
-    assertEquals(0, calculator.subtract(2, 2), 0);
-    assertEquals(4.86, calculator.subtract(3.14, -1.72), 0.001);
-    assertEquals(2.0/3, calculator.subtract(1.0/3, -1.0/3), 0.001);
-}
+    @Test
+    public void addTest() {
+        assertEquals(4, calculator.add(2, 2), 0);
+        assertEquals(1.42, calculator.add(3.14, -1.72), 0.001);
+        assertEquals(5.0 / 3, calculator.add(2.5 / 3, 2.5 / 3), 0.001);
+    }
 
-@Test
-public void multiply() {
-    assertEquals(2, calculator.multiply(1, 2), 0);
-    assertEquals(-5.4008, calculator.multiply(3.14, -1.72), 0.001);
-    assertEquals(0.111, calculator.multiply(1.0/3, 1.0/3), 0.001);
-}
+    @Test
+    public void subtractTest() {
+        assertEquals(0, calculator.subtract(2, 2), 0);
+        assertEquals(4.86, calculator.subtract(3.14, -1.72), 0.001);
+        assertEquals(2.0 / 3, calculator.subtract(1.0 / 3, -1.0 / 3), 0.001);
+    }
 
-@Test
-public void divide() {
-    assertEquals(2, calculator.divide(4, 2), 0);
-    assertEquals(-1.826, calculator.divide(3.14, -1.72), 0.001);
-    assertEquals(1, calculator.divide(1.0/3, 1.0/3), 0);
-}
+    @Test
+    public void multiplyTest() {
+        assertEquals(2, calculator.multiply(1, 2), 0);
+        assertEquals(-5.4008, calculator.multiply(3.14, -1.72), 0.001);
+        assertEquals(0.111, calculator.multiply(1.0 / 3, 1.0 / 3), 0.001);
+    }
 
-@Test
-public void divideException() {
-    Assertions.assertThrows(ArithmeticException.class, new Executable() {
+    @Test
+    public void divideTest() {
+        assertEquals(2, calculator.divide(4, 2), 0);
+        assertEquals(-1.826, calculator.divide(3.14, -1.72), 0.001);
+        assertEquals(1, calculator.divide(1.0 / 3, 1.0 / 3), 0);
+    }
 
-        @Override
-        public void execute() throws Throwable {
+    @Test
+    public void divideExceptionTest() {
+        Assertions.assertThrows(ArithmeticException.class, new Executable() {
 
-            calculator.divide(5, 0);
-
+            @Override
+            public void () =>calculator.divide(5,0);
         }
-    });
-}
-
+    }
 }
