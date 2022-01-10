3248031df4ce356eda729d7cb646e334c1362da1
IlyaKolbasov <51168504+IlyaKolbasov@users.noreply.github.com>
2022-01-10 21:22:27 +0300

    Delete CalculatorService.java

--- kolbasov/lesson01/homework/src/main/java/CalculatorService.java
+++
@@ -1,9 +0,0 @@
-public interface CalculatorService {
-    double add(double a, double b);
-
-    double subtract(double a, double b);
-
-    double multiply(double a, double b);
-
-    double divide(double a, double b) throws DivisionByZeroException;
-}
