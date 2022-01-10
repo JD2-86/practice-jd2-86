7c7fca58483dc576e555b686cea389cf90688015
Alexey Paramonov <paramonov_tama@mail.ru>
2022-01-10 21:29:34 +0300

    practice 12 lesson

---
+++ paramonov/practice/lesson12/JPA/src/main/java/by/MessageEntity.java
@@ -0,0 +1,15 @@
+package by;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageEntity {
+    private int id;
+    private String text;
+}
