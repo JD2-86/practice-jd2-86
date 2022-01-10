d39d5217ace73209ac27be25b380864b24989391
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-01-10 13:24:43 +0300

    feat: add language level table

--- shcharbunou/lesson08-09/learning_management_system_database.sql
+++ shcharbunou/lesson08-09/learning_management_system_database.sql
@@ -1,4 +1,21 @@
 CREATE DATABASE learning_management_system_database;
 
+DROP SCHEMA IF EXISTS user_schema;
+
 CREATE SCHEMA user_schema;
+
+CREATE TABLE user_schema.language_proficiency_level
+(
+    level_id SERIAL PRIMARY KEY,
+    level_designation VARCHAR(32) NOT NULL
+);
+
+INSERT INTO user_schema.language_proficiency_level (level_designation)
+VALUES ('A1'),
+       ('A2'),
+       ('B1'),
+       ('B2'),
+       ('C1'),
+       ('C2');
+
 
