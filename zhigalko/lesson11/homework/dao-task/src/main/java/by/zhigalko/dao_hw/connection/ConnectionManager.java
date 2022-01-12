7d8cab3662d46e044fd9c71620153843f5e19a67
Evgeniy Zhigalko <evgeniy.zhigalko@gmail.com>
2022-01-12 14:11:40 +0300

    fix: fix link to aws and jdbc connect

--- zhigalko/lesson11/homework/dao-task/src/main/java/by/zhigalko/dao_hw/connection/ConnectionManager.java
+++ zhigalko/lesson11/homework/dao-task/src/main/java/by/zhigalko/dao_hw/connection/ConnectionManager.java
@@ -5,13 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    private static final String DATASOURCE_URL = "jdbc:postgresql://ec2-54-144-232-209.compute-1.amazonaws.com:5432/course_java_data_persistence";
+    private static final String DATASOURCE_URL = "jdbc:postgresql://ec2-54-165-213-86.compute-1.amazonaws.com:5432/course_java_data_persistence";
     private static final String DATASOURCE_USER = "postgres";
     private static final String DATASOURCE_PASSWORD = "1234";
     private static volatile ConnectionManager instance = null;
 
     private ConnectionManager() {}
-
 
     public static ConnectionManager getInstance() {
         if (instance == null) {
