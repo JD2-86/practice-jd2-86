5bc59378266325bc7fde40a789a43cc6662dc5dc
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-01-17 09:39:39 +0300

    feat: add connection manager with pgsimpledatasource

---
+++ shcharbunou/lesson10-11/music-store/src/main/java/by/shcharbunou/musicstore/dao/connection/ConnectionManager.java
@@ -0,0 +1,36 @@
+package by.shcharbunou.musicstore.dao.connection;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private static final String DATASOURCE_URL = "jdbc:postgresql://localhost:5432/course_java_data_persistence";
+    private static final String DATASOURCE_USER = "postgres";
+    private static final String DATASOURCE_PASSWORD = "12345678";
+    private static ConnectionManager instance = null;
+
+    private ConnectionManager() {}
+
+    public static ConnectionManager getInstance() {
+        ConnectionManager localInstance = instance;
+        if (localInstance == null) {
+            synchronized (ConnectionManager.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new ConnectionManager();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        PGSimpleDataSource source = new PGSimpleDataSource();
+        source.setURL(DATASOURCE_URL);
+        source.setUser(DATASOURCE_USER);
+        source.setPassword(DATASOURCE_PASSWORD);
+        return source.getConnection();
+    }
+}
