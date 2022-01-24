40158f0cdb2212c02b7160f9648487eb9633f5f0
Alexey Paramonov <paramonov_tama@mail.ru>
2022-01-24 20:47:29 +0300

    add: classworks on pc

--- paramonov/practice/lesson15/src/main/java/by/paramonov/tpc/PersistentEmployeeTpc.java
+++ paramonov/practice/lesson15/src/main/java/by/paramonov/tpc/PersistentEmployeeTpc.java
@@ -1,6 +1,8 @@
 package by.paramonov.tpc;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDate;
@@ -12,7 +14,12 @@ import java.time.LocalDate;
         @AttributeOverride(name = "id_tpc_employee", column = @Column(name = "contract_employee_id")),
         @AttributeOverride(name = "first_name", column = @Column(name = "first_name")),
         @AttributeOverride(name = "last_name", column = @Column(name = "last_name"))
+})
 public class PersistentEmployeeTpc extends EmployeeTpc {
+
+    @Getter
+    @Setter
+
     private LocalDate startDate;
     private LocalDate liveDate;
-    }
+}
